e221f9d1a264f938cc88fa89527d2c3e86f60a7f
krother <krother@academis.eu>
2017-05-30 11:06:21 +0200

    created walkthrough

---
+++ walkthrough.py
@@ -0,0 +1,19 @@
+
+from unittest.mock import patch
+from space import SpaceGame, create_galaxy
+import views
+
+COMPLETE_SOLUTION = iter(open('solution.txt').read())
+views.SKIP_INPUT = True
+views.SLOW_MOTION = True
+
+
+@patch('builtins.input', lambda x:next(COMPLETE_SOLUTION))
+def show_walkthrough():
+    galaxy = create_galaxy()
+    sg = SpaceGame(galaxy)
+    sg.travel()
+
+
+if __name__ == '__main__':
+    show_walkthrough()

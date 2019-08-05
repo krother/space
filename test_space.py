273c32c54ef7d9b82aa797578656b2461369b456
Kristian Rother <krother@academis.eu>
2019-08-05 22:58:40 +0200

    add fixture, remove oldschool test class

--- test_space.py
+++ test_space.py
@@ -1,10 +1,10 @@
 
-from unittest import TestCase
 from unittest.mock import patch
 from space import SpaceGame, create_galaxy
 from locations import AncientShipwreck
 from ships import Spaceship
 import views
+import pytest
 
 PICKUP = iter('Fx')
 MOVE = iter('1x')
@@ -14,33 +14,34 @@ COMPLETE_SOLUTION = iter(open('solution.txt').read())
 
 views.SKIP_INPUT = True
 
+@pytest.fixture
+def galaxy():
+    return create_galaxy()
 
-class SpaceTests(TestCase):
 
-    def setUp(self):
-        self.galaxy = create_galaxy()
+class TestSpace:
 
     @patch('builtins.input', lambda x: next(PICKUP))
-    def test_pickup(self):
-        sg = SpaceGame(self.galaxy)
+    def test_pickup(self, galaxy):
+        sg = SpaceGame(galaxy)
         sg.travel()
         assert sg.ship.cargo == 'food'
 
     @patch('builtins.input', lambda x: next(MOVE))
-    def test_warp(self):
-        sg = SpaceGame(self.galaxy)
+    def test_warp(self, galaxy):
+        sg = SpaceGame(galaxy)
         sg.travel()
         assert sg.ship.planet.name == 'Centauri'
 
     @patch('builtins.input', lambda x: next(TRIPLE_JUMP))
-    def test_triple_warp(self):
-        sg = SpaceGame(self.galaxy)
+    def test_triple_warp(self, galaxy):
+        sg = SpaceGame(galaxy)
         sg.travel()
         assert sg.ship.planet.name == 'New Haven'
 
     @patch('builtins.input', lambda *args: next(FIRST_ARTIFACT))
-    def test_pickup_artifact(self):
-        sg = SpaceGame(self.galaxy)
+    def test_pickup_artifact(self, galaxy):
+        sg = SpaceGame(galaxy)
         sg.travel()
         assert sg.ship.artifacts == 1
 
@@ -56,7 +57,7 @@ class SpaceTests(TestCase):
         assert ship.artifacts == 1
 
     @patch('builtins.input', lambda x: next(COMPLETE_SOLUTION))
-    def test_finish_game(self):
-        sg = SpaceGame(self.galaxy)
+    def test_finish_game(self, galaxy):
+        sg = SpaceGame(galaxy)
         sg.travel()
         assert sg.solved

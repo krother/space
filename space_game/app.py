195666d1b238df6191f36afb80f71f7d0038f182
Kristian Rother <krother@academis.eu>
2024-04-21 22:48:29 +0200

    slimmer app

--- space_game/app.py
+++ space_game/app.py
@@ -1,74 +1,20 @@
-from typing import Any, Callable, Optional
+from typing import Any, Optional
 
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from jinja2 import Environment, FileSystemLoader, Template, TemplateNotFound
 from pydantic import BaseModel
-from space_game.middleware import PreserveJSONResponse
+from space_game.middleware import PreserveJSONResponse, json_to_html
 
 # libs for testing
 from faker import Faker
 import random
 
-# Create the FastAPI application.
 app = FastAPI(default_response_class=PreserveJSONResponse)
 
-
-# Set up Jinja to be able to render templates.
-jinja_env = Environment(
-    loader=FileSystemLoader("templates"),
-)
-
-
-# We define a middleware that converts responses to HTML using a Jinja template
-# based on the endpoint function's name.
-def get_template(endpoint: Callable | None) -> Template | None:
-    """Get Jinja template instance for a given path operation."""
-    if endpoint is None:
-        # We don't know the function that handled this request, therefore we
-        # can't select a corresponding template.
-        return None
-
-    # Simply add `.html` to the function name and try to look this up as a
-    # template file.
-    try:
-        return jinja_env.get_template(f"{endpoint.__name__}.html")
-    except TemplateNotFound:
-        return None
-
-
-@app.middleware("http")
-async def json_to_html(request: Request, call_next):
-    # Compute the response.
-    response = await call_next(request)
-
-    # Check whether the request is sent by HTMX. It always sets this header, see
-    # <https://htmx.org/docs/#request-headers>.
-    is_hx = request.headers.get("HX-Request", "") == "true"
-
-    # Check whether the client requested HTML. This is just a proof of concept,
-    # more solid `Accept` header parsing would be required.
-    wants_html = request.headers.get("Accept", "") == "text/html"
-
-    # Whether the request should be rendered as HTML or not.
-    should_render = is_hx or wants_html
-
-    if (
-        should_render
-        # Has PreserveJSONResponse kept the original Python object for us?
-        and "preserved_json_data" in request.scope
-        # Is there a Jinja template defined for this endpoint?
-        and (template := get_template(request.scope.get("endpoint")))
-    ):
-        return HTMLResponse(
-            template.render(
-                data=request.scope["preserved_json_data"],
-            )
-        )
-
-    # No HTML rendering, return the original response unchanged.
-    return response
+# register middleware that automatically uses JINJA templates
+# so we only have to write REST functions
+# *** Kudos to Tim Weber for finding out! ***
+app.middleware("http")(json_to_html)
 
 
 class LocationData(BaseModel):
@@ -92,7 +38,7 @@ def new_game() -> GameData:
     return GameData(
         game_id="1234",
         location=LocationData(name="Pandalor", image="pandalor", description="a thick bamboo forest"),
-        cargo="medical",
+        cargo="bamboo",
         commands=["one", "two", "three"],
     )
 
@@ -104,10 +50,10 @@ def action(game_id: str, command: str) -> GameData:
         game_id=game_id,
         location=LocationData(
             name=f.city(),
-            image=random.choice(["pandalor", "rorke", "magminus", "vega"]),
+            image=random.choice(["pandalor", "adalov", "colabo", "valuerro"]),
             description=f.sentence(),
         ),
-        cargo=random.choice(["medical", "food", "gas", "minerals", "nucleons"]),
+        cargo=random.choice(["peanuts", "starmap", "bamboo", "dna"]),
         crew=["panda"] + [random.choice(["hamster", "python", "pingu", "unicorn", "elephant"]) for _ in range(5)],
         commands=[f.word() for _ in range(4)],
         message="done: " + command,
--- space_game/middleware.py
+++ space_game/middleware.py
@@ -1,4 +1,9 @@
+from typing import Callable
+
 from fastapi.responses import JSONResponse
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from jinja2 import Environment, FileSystemLoader, Template, TemplateNotFound
 
 from typing import Any
 
@@ -25,3 +30,58 @@ class PreserveJSONResponse(JSONResponse):
         # a really good candidate ;)
         scope["preserved_json_data"] = self.original_data
         return super().__call__(scope, receive, send)
+
+
+# Set up Jinja to be able to render templates.
+jinja_env = Environment(
+    loader=FileSystemLoader("templates"),
+)
+
+
+# We define a middleware that converts responses to HTML using a Jinja template
+# based on the endpoint function's name.
+def get_template(endpoint: Callable | None) -> Template | None:
+    """Get Jinja template instance for a given path operation."""
+    if endpoint is None:
+        # We don't know the function that handled this request, therefore we
+        # can't select a corresponding template.
+        return None
+
+    # Simply add `.html` to the function name and try to look this up as a
+    # template file.
+    try:
+        return jinja_env.get_template(f"{endpoint.__name__}.html")
+    except TemplateNotFound:
+        return None
+
+
+async def json_to_html(request: Request, call_next):
+    # Compute the response.
+    response = await call_next(request)
+
+    # Check whether the request is sent by HTMX. It always sets this header, see
+    # <https://htmx.org/docs/#request-headers>.
+    is_hx = request.headers.get("HX-Request", "") == "true"
+
+    # Check whether the client requested HTML. This is just a proof of concept,
+    # more solid `Accept` header parsing would be required.
+    wants_html = request.headers.get("Accept", "") == "text/html"
+
+    # Whether the request should be rendered as HTML or not.
+    should_render = is_hx or wants_html
+
+    if (
+        should_render
+        # Has PreserveJSONResponse kept the original Python object for us?
+        and "preserved_json_data" in request.scope
+        # Is there a Jinja template defined for this endpoint?
+        and (template := get_template(request.scope.get("endpoint")))
+    ):
+        return HTMLResponse(
+            template.render(
+                data=request.scope["preserved_json_data"],
+            )
+        )
+
+    # No HTML rendering, return the original response unchanged.
+    return response
